6adaaba0a6f99e38b202f02487a4654d2bb31d89
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-07 12:33:01 -0400

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Client.Wait.MixProject do
   def project do
     [
       app: :islands_client_wait,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Client Wait",
